f52da58f08e1125ee11b99c44f61afdd73a537b7
Antoine Neuraz <aneuraz@users.noreply.github.com>
2021-12-16 15:15:33 +0100

    add dataset converter and datasets

--- brat2conll.py
+++ brat2conll.py
@@ -1,7 +1,7 @@
 from brat import load_from_brat
 from spacy.lang.fr import French
 import pandas as pd    
-
+import random
 
 class Brat2Conll: 
     def __init__(self):
@@ -36,14 +36,47 @@ class Brat2Conll:
         return res
     
     def add_entities_to_df(self, d, doc, df):
-        for ent in doc['entities']:
-            for fragment in ent['fragments']:
-                span = d.char_span(fragment['begin'], fragment['end'], ent['label'],alignment_mode="expand")
-                df.iloc[span.start:span.end,3] = ent['label']
+        if 'entities' in doc.keys():
+            for ent in doc['entities']:
+                for fragment in ent['fragments']:
+                    span = d.char_span(fragment['begin'], fragment['end'], ent['label'],alignment_mode="expand")
+                    df.iloc[span.start:span.end,3] = ent['label']
         return df
+
     
     
     def convert(self, input_dir:str):
         brat_files = list(load_from_brat(input_dir))
         df = [self.brat_to_df(doc) for doc in brat_files]
         return df
+    
+    
+def split_train_test(df_agg_filtered, seed = 23, train_frac = 0.8):
+
+    random.seed(seed)
+    unique_ids = df_agg_filtered['doc_id'].unique()
+    random.shuffle(unique_ids)
+
+    max_train_position = round(unique_ids.shape[0] * train_frac)
+    
+    train_ids = unique_ids[:max_train_position]
+    test_ids = unique_ids[max_train_position:]
+    
+    trainset = df_agg_filtered.loc[df_agg_filtered['doc_id'].isin(train_ids)]
+    testset = df_agg_filtered.loc[df_agg_filtered['doc_id'].isin(test_ids)]
+    
+    trainset = dataframe_to_list(trainset)
+    testset = dataframe_to_list(testset)
+    
+    return trainset, testset
+
+def dataframe_to_list(dataset):
+    dataset_list = []
+    for index, row in dataset.iterrows():
+        dataset_list.append({
+            'doc_id' : row[0],
+            'sent_id': row[1],
+            'token': row[2],
+            'label': row[3]
+        })
+    return dataset_list
